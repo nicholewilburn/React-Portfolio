2fabfb32e978c4031cb82373970f225bf26f9232
Nichole <nicholewilburn@gmail.com>
2023-09-26 13:57:34 -0500

    portfolio page looking good

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Project({ project }) {
   const { title, image, deployedLink, githubLink } = project;
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="project">
-      <h3>{title}</h3>
-      <img src={image} alt={title} />
-      <div className="project-links">
-        <a href={deployedLink} target="_blank" rel="noopener noreferrer">
-          Live Demo
-        </a>
-        <a href={githubLink} target="_blank" rel="noopener noreferrer">
-          GitHub Repository
-        </a>
+    <div className={`project ${isHovered ? 'hovered' : ''}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="project-info">
+        <h3>{title}</h3>
+        <div className={`project-links ${isHovered ? 'show' : ''}`}>
+          <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+            Live Demo
+          </a>
+          <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            GitHub Repository
+          </a>
+        </div>
       </div>
+      <img src={`/images/${image}`} alt={title} className="project-image" />
     </div>
   );
 }
